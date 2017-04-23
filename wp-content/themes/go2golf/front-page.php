6df61e24e24fa13e45d5f0544c0ccd7e89285fa7
James Tudsbury <james.tudsbury1@gmail.com>
2017-04-23 09:42:58 +0100

    Snags

--- wp-content/themes/go2golf/front-page.php
+++ wp-content/themes/go2golf/front-page.php
@@ -5,8 +5,8 @@
 	<main role="main">
 		<article class="homepage-hero">
 			<div class="homepage-hero__content">
-				<h1 class="homepage-hero__heading">Find the course for you</h1>
-				<h2 class="homepage-hero__subheading">Subheading</h2>
+				<h1 class="homepage-hero__heading">Find the golf course for you</h1>
+				<h2 class="homepage-hero__subheading">Golf course information and independent reviews</h2>
 				<?php get_sidebar(); ?>
 			</div><!--/.homepage-hero__content -->
 		</article>
