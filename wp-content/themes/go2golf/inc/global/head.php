9e11138ea6d41be87a3ac5294c51960560341d01
James Tudsbury <james.tudsbury1@gmail.com>
2017-05-28 17:28:16 +0100

    GTM

--- wp-content/themes/go2golf/inc/global/head.php
+++ wp-content/themes/go2golf/inc/global/head.php
@@ -22,6 +22,14 @@
 
 <?php wp_head(); ?>
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PD92CDK');</script>
+<!-- End Google Tag Manager -->
+
 <!-- Make our theme path accessible in Javascript -->
 <script>var getThemePath = '<?php echo getThemePath(); ?>';</script>
 
