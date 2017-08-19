d81c7a47c90f7ccf0ebf98d7c16a3fa26fe73c71
James Tudsbury <james.tudsbury1@gmail.com>
2017-08-19 11:50:57 +0100

    Product single

--- wp-content/themes/go2golf/dist/js/global.js
+++ wp-content/themes/go2golf/dist/js/global.js
@@ -2,20 +2,57 @@
 'use strict';
 
 	function stickySidebar() {
-		setTimeout(function() {
-			if (window.innerWidth >= 960) {
-				$('[data-id="product-cta-sidebar"]').stick_in_parent(
-					{
-						"parent": $('body'),
-						"offset_top": 20
-					}
-				);
-			} else {
-				$('[data-id="product-cta-sidebar"]').trigger("sticky_kit:detach");
-			}
-		}, 500);
+		if (window.innerWidth >= 960) {
+			$('[data-id="product-cta-sidebar"]').stick_in_parent(
+				{
+					"parent": $('body'),
+					"offset_top": 20
+				}
+			);
+		} else {
+			$('[data-id="product-cta-sidebar"]').trigger("sticky_kit:detach");
+		}
+	}
+	stickySidebar();
+
+	function productSidebarToggleReveal() {
+		if (window.innerWidth <= 670) {
+			$('[data-id="product-cta-sidebar"]').css('max-height', '60px');
+			var lastScrollTop = (typeof lastScrollTop === 'undefined') ? 0 : lastScrollTop; 
+			$(window).scroll(function(event){
+				var st = $(this).scrollTop();
+				if (st > lastScrollTop){
+					$('[data-id="product-cta-sidebar"]').css('max-height', '60px');
+				} else {
+					$('[data-id="product-cta-sidebar"]').css('max-height', '100vh');
+				}
+				lastScrollTop = st;
+			});
+		} else {
+			$(window).scroll(function(event){
+				$('[data-id="product-cta-sidebar"]').css('max-height', '100vh');
+			});
+			$('[data-id="product-cta-sidebar"]').css('max-height', '100vh');
+		}
+	}
+	productSidebarToggleReveal();
+
+	function executeWindowResizeFunctions() {
+		clearTimeout(resizeTimer);
+		resizeTimer = setTimeout(function() {
+			stickySidebar();
+			productSidebarToggleReveal();
+		}, 1000);
 	}
 
-	window.addEventListener('resize', stickySidebar);
+	var resizeTimer;
+	var cachedWidth = $(window).width();
+	window.addEventListener('resize', function() {
+		var newWidth = $(window).width();
+        if(newWidth !== cachedWidth){
+            executeWindowResizeFunctions();
+            cachedWidth = newWidth;
+        }
+	});
 
 })(window);
