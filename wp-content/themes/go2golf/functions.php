5a893062e84cc12f8f456ac2e9301c6624cbfb97
Steve McKinney <steve@iamsteve.me>
2017-12-03 14:58:53 +0000

    single post styles

--- wp-content/themes/go2golf/functions.php
+++ wp-content/themes/go2golf/functions.php
@@ -33,6 +33,7 @@ if (function_exists('add_theme_support'))
 
     // Add Thumbnail Theme Support
     add_theme_support('post-thumbnails');
+    add_image_size('single', 1280, 640, true); // Large Thumbnail
     add_image_size('large', 700, '', true); // Large Thumbnail
     add_image_size('medium', 250, '', true); // Medium Thumbnail
     add_image_size('small', 120, '', true); // Small Thumbnail
