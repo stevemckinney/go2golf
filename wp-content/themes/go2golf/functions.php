ae4b68590c7904cf152d5185aca736489c529e85
Steve McKinney <steve@iamsteve.me>
2018-02-25 12:00:35 +0000

    add footer menu

--- wp-content/themes/go2golf/functions.php
+++ wp-content/themes/go2golf/functions.php
@@ -103,7 +103,8 @@ function register_html5_menu()
     register_nav_menus(array( // Using array to specify more menus if needed
         'header-menu' => __('Header Menu', 'html5blank'), // Main Navigation
         'sidebar-menu' => __('Sidebar Menu', 'html5blank'), // Sidebar Navigation
-        'extra-menu' => __('Extra Menu', 'html5blank') // Extra Navigation if needed (duplicate as many as you need!)
+        'extra-menu' => __('Extra Menu', 'html5blank'), // Extra Navigation if needed (duplicate as many as you need!)
+        'footer-menu' => __('Footer Menu', 'html5blank'),
     ));
 }
 
